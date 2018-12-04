c1b63be8e2c94a79f8f98df75a4f8b061a373d2e
TGA <tabakaevga@gmail.com>
2018-12-05 01:07:16 +0300

    Удален лишний файл

--- ProjectUnderTest/src/UnitTests/VectorTests.java
+++ ProjectUnderTest/src/UnitTests/VectorTests.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
 
 import VectorAndStack.Vector;
